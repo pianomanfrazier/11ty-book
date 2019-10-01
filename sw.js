897ac5932fb09fa8f92a2b96463b1f94cb29f12f
Ryan Frazier <pianomanfrazier@gmail.com>
2019-10-01 00:01:08 -0600

    Add service worker and workbox scripts

---
+++ sw.js
@@ -0,0 +1,34 @@
+importScripts('https://storage.googleapis.com/workbox-cdn/releases/3.6.1/workbox-sw.js');
+
+if (workbox) {
+    console.log(`Yay! Workbox is loaded 🎉`);
+    // Cache the Google Fonts stylesheets with a stale while revalidate strategy.
+
+    workbox.precaching.precacheAndRoute([]);
+
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.googleapis\.com/,
+        workbox.strategies.staleWhileRevalidate({
+            cacheName: 'google-fonts-stylesheets',
+        }),
+    );
+
+    // Cache the Google Fonts webfont files with a cache first strategy for 1 year.
+    workbox.routing.registerRoute(
+        /^https:\/\/fonts\.gstatic\.com/,
+        workbox.strategies.cacheFirst({
+            cacheName: 'google-fonts-webfonts',
+            plugins: [
+                new workbox.cacheableResponse.Plugin({
+                    statuses: [0, 200],
+                }),
+                new workbox.expiration.Plugin({
+                    maxAgeSeconds: 60 * 60 * 24 * 365,
+                }),
+            ],
+        }),
+    );
+
+} else {
+    console.log(`Boo! Workbox didn't load 😬`);
+}
---
+++ workbox-config.js
@@ -0,0 +1,8 @@
+module.exports = {
+    "globDirectory": "_site/",
+    "globPatterns": [
+        "**/*.{html,css,js}"
+    ],
+    "swDest": "_site/sw.js",
+    "swSrc": "./sw.js"
+};

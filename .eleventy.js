309914b65fac6de739f2abcb8ec0d992027b218e
Ryan Frazier <pianomanfrazier@gmail.com>
2019-10-01 09:00:43 -0600

    Add service worker to build

--- .eleventy.js
+++ .eleventy.js
@@ -85,6 +85,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
+  eleventyConfig.addPassthroughCopy("sw.js");
   eleventyConfig.addPassthroughCopy("favicon.ico");
 
   /* Markdown Plugins */

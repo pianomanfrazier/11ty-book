78b450f4a826cb02b99563acab7959fb73ed1aa0
Ryan Frazier <pianomanfrazier@gmail.com>
2019-09-30 23:52:34 -0600

    Add favicon and icons

--- .eleventy.js
+++ .eleventy.js
@@ -90,7 +90,7 @@ module.exports = function (eleventyConfig) {
   eleventyConfig.addPassthroughCopy("img");
   eleventyConfig.addPassthroughCopy("css");
   eleventyConfig.addPassthroughCopy("js");
-  // eleventyConfig.addPassthroughCopy("favicon.ico");
+  eleventyConfig.addPassthroughCopy("favicon.ico");
 
   /* Markdown Plugins */
   const markdownIt = require("markdown-it");

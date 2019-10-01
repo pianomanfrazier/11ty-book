831a1a2a27cd9760f7a3e3ed3c0aa388904f4769
Ryan Frazier <pianomanfrazier@gmail.com>
2019-10-01 09:30:58 -0600

    Remove local storage nav. It looks wierd.

--- _includes/menu.js
+++ _includes/menu.js
@@ -2,34 +2,19 @@ var mainNav = document.getElementById('main-nav');
 var navPrev = document.getElementsByClassName('nav-prev');
 var mainContent = document.getElementById('main-content');
 var hamburger = document.getElementById('hamburger');
-
-if (!localStorage.getItem('nav')) {
-  localStorage.setItem('nav', 'off');
-} else if (localStorage.getItem('nav') === 'on') {
-  mainNav.classList.add('active');
-  hamburger.classList.add('is-active');
-  mainContent.classList.add('active');
-  if (navPrev[0]) {
-    navPrev[0].classList.add('active');
-  }
-}
-
+var toggle = 'off';
 function toggleMainNav() {
-  if (localStorage.getItem('nav') === 'off') {
+  if (toggle === 'off') {
     hamburger.classList.add('is-active');
     mainNav.classList.add('active');
     mainContent.classList.add('active');
-    if (navPrev[0]) {
-      navPrev[0].classList.add('active');
-    }
-    localStorage.setItem('nav', 'on');
+    if (navPrev[0]) navPrev[0].classList.add('active');
+    toggle = 'on';
   } else {
     hamburger.classList.remove('is-active');
     mainNav.classList.remove('active');
     mainContent.classList.remove('active');
-    if (navPrev[0]) {
-      navPrev[0].classList.remove('active');
-    }
-    localStorage.setItem('nav', 'off');
+    if (navPrev[0]) navPrev[0].classList.remove('active');
+    toggle = 'off';
   }
 }
